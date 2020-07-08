d60fe37e229bdc74620ecb40e82aad9640ebf1fe
Karthik <namburu.karthik@gmail.com>
2020-07-07 21:34:14 -0400

    New Commit

--- src/index.js
+++ src/index.js
@@ -4,25 +4,24 @@ import ReactDOM from 'react-dom';
 const rootNode = document.getElementById('root');
 
 function App() {
-	const GITHUB_URL = 'https://api.github.com/users/';
+	const GITHUB_URL = 'https://api.github.com/users/karthiknamburu';
 	const [userProfile, setUserProfile] = useState(null);
-	const [search, setSearch] = useState('karthiknamburu');
-
+	
 	async function getUser() {
-		const response = await fetch(`${GITHUB_URL}${search}`);
+		const response = await fetch(GITHUB_URL);
 		const data = await response.json();
 		setUserProfile(data);
 	}
 
 	useEffect(() => {
 		getUser();
-	}, [search]);
+	}, []);
 
 	return userProfile ? (
 		<div>
 			<h1>Github Profile</h1>
-			<input type='text' onChange={(event) => setSearch(event.target.value)} />
-			<button onClick={getUser}>Search</button>
+			<input type='text' />
+			{/* <button onClick={}>Search</button> */}
 			<button>Clear</button>
 			<ul>
 				<li>{userProfile.login}</li>
